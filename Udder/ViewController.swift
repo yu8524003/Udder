a419c2de170e520ec6fbf5f367f3fd20298009f9
mewregi <mewregi@gmail.com>
2018-10-18 19:57:29 -0700

    Add view will appear.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        print("great")
+    }
 
 }
 
